266c74617ed9b5c2bcb1a1fa753c66b9b2890f9e
Michał Jezierski <ambinanitelo@gmail.com>
2017-02-21 13:17:21 +0100

    editorconfig

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ module.exports.cache = curry((config, target, key, descriptor) => {
     const fn = descriptor.value;
 
     descriptor.state = {};
-    
     descriptor.value = function () {
         const key = config.key;
         const state = descriptor.state;
