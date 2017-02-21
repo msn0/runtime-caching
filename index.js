a38fac9af8a79a779fa207503b085865a88058ef
Michał Jezierski <ambinanitelo@gmail.com>
2017-02-21 12:58:46 +0100

    working, naive version

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const curry = require('dead-simple-curry');
 
-const state = {};
-
 module.exports.cache = curry((config, target, key, descriptor) => {
 
     const fn = descriptor.value;
 
+    descriptor.state = {};
+    
     descriptor.value = function () {
+        console.log(1, descriptor);  
         const key = { config };
+        const state = { descriptor };
+        console.log(descriptor.state[key]);
         if (state.hasOwnProperty(key)) {
+
             return state[key];
         }
-        state[key] = fn.apply(target, arguments);
+        descriptor.state[key] = fn.apply(target, arguments);
     };
     return descriptor;
 });
