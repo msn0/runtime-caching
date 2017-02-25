6e3005558e4772c32cc4dfec19a5e75a591749ca
Michał Jezierski <ambinanitelo@gmail.com>
2017-02-25 16:18:57 +0100

    some comments

--- index.js
+++ index.js
@@ -8,10 +8,13 @@ module.exports.cache = curry((config, context, key, descriptor) => {
         this.___cache = this.___cache || {};
 
         const index = `${key}___${JSON.stringify(arguments)}`;
+
+        // return cache entry if any
         if (this.___cache[index] !== undefined) {
             return this.___cache[index];
         }
 
+        // cache response
         this.___cache[index] = fn.apply(context, arguments);
 
         // schedule deletion
