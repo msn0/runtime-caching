6dd3bb5b14de316c7f68e760325528954197d27c
Michał Jezierski <ambinanitelo@gmail.com>
2017-03-02 14:50:19 +0100

    redux initial

--- demo/app.js
+++ demo/app.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducer';
 import Suggester from './suggester';
 
+const store = createStore(reducer);
+
 render(
-    <section>
-        <header>
-            <h1>Google Books</h1>
-        </header>
-        <Suggester />
-    </section>,
+    <Provider store={ store }>
+        <section>
+            <header>
+                <h1></h1>
+            </header>
+            <Suggester />
+        </section>
+    </Provider>,
     document.getElementById('root')
 );
--- demo/suggester.js
+++ demo/suggester.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { changePhrase } from './actions';
 
-export default function () {
-    return <div>suggester</div>;
-}
+const Suggester = (state) => {
+
+    console.log('sss', state);
+
+    function change() {
+        console.log('change');
+        state.dispatch(changePhrase('test phrase'));
+    }
+
+    return (
+        <div>
+            <button onClick={ change }>
+                click me
+            </button>
+            <div>Phrase: { state.phrase }</div>
+        </div>
+    );
+};
+
+const mapStateToProps = ({ phrase }) => ({ phrase });
+
+export default connect(mapStateToProps)(Suggester);
