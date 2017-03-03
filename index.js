743c2dcd36e6411bd3269e763af686edf81c5f95
Michał Jezierski <ambinanitelo@gmail.com>
2017-03-03 12:03:47 +0100

    cache functions

--- index.js
+++ index.js
@@ -1,6 +1,49 @@
+function cacheFunction (context, config) {
+    const fn = context;
+
+    context = function () {
+        context.prototype.___cache = context.prototype.___cache || {};
+
+        const index = `s___${JSON.stringify(arguments)}`;
+
+        // return cache entry if any
+        if (context.prototype.___cache[index] !== undefined) {
+            return context.prototype.___cache[index];
+        }
+
+        // cache response
+        const result = fn.apply(context.prototype, arguments);
+
+        // if (result.constructor === Promise) {
+        //     return result.then(data => {
+        //         this.___cache[index] = new Promise(resolve => resolve(data));
+        //
+        //         // schedule deletion
+        //         setTimeout(() => delete this.___cache[index], config.timeout);
+        //
+        //         return Promise.resolve(data);
+        //     });
+        // }
+
+        // persist cache entry
+        context.prototype.___cache[index] = result;
+
+        // schedule deletion
+        setTimeout(() => delete context.prototype.___cache[index], config.timeout);
+
+        return context.prototype.___cache[index];
+    };
+
+    return context;
+}
+
 module.exports.cache = (config) => {
 
     return function (context, key, descriptor) {
+
+        if (typeof context === 'function') {
+            return cacheFunction(context, config);
+        }
 
         const fn = descriptor.value;
 
--- test/test-function.js
+++ test/test-function.js
@@ -1,13 +1,48 @@
 import test from 'ava';
+import sinon from 'sinon';
 import { cache } from '../';
 
-test('should call annotated method only once', t => {
-    const foo = cache({ timeout: 1000 })(() => {
-        return 'foo value';
-    });
+test('the value for subsequent calls should match', t => {
+    const foo = cache({ timeout: 1000 })(Math.random);
 
     const first = foo();
     const second = foo();
 
     t.is(first, second);
+});
+
+test('the value for subsequent calls should match (with arguments)', t => {
+    const foo = cache({ timeout: 1000 })(Math.random);
+
+    const first = foo('1', { foo: 'bar' });
+    const second = foo('1', { foo: 'bar' });
+
+    t.is(first, second);
+});
+
+test('value should not be shared across instances', t => {
+    function bar() {
+        return Math.random();
+    }
+    const foo = cache({ timeout: 1000 })(bar);
+
+    const first = new foo();
+    const second = new foo();
+
+    t.not(first, second);
+});
+
+test('should call cached function only once', t => {
+    const fakeObject = {
+        bar: function () {
+            return Math.random();
+        }
+    };
+    const spy = sinon.spy(fakeObject, 'bar');
+    const foo = cache({ timeout: 1000 })(fakeObject.bar);
+
+    foo();
+    foo();
+
+    t.truthy(spy.called);
 });
