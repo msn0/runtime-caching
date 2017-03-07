d6ca6bf8d6e994738b6674652ede810e73c477a6
Michał Jezierski <ambinanitelo@gmail.com>
2017-03-07 15:22:03 +0100

    should cache value (with arguments)

--- test/function-promise.js
+++ test/function-promise.js
@@ -1,12 +1,8 @@
 import test from 'ava';
 import { cache } from '../';
 
-test('should call cached function only once', async t => {
-    const foobar = cache({ timeout: 1000 })(function foo() {
-        return new Promise((resolve) =>
-            setTimeout(() => resolve(Math.random()), 100)
-        );
-    });
+test('should cache value', async t => {
+    const foobar = cache({ timeout: 1000 })(foo);
 
     const first = await foobar();
     const second = await foobar();
@@ -14,15 +10,26 @@ test('should call cached function only once', async t => {
     t.is(first, second);
 });
 
-test('should call twice when arguments differ', async t => {
-    const foobar = cache({ timeout: 1000 })(function foo() {
-        return new Promise((resolve) =>
-            setTimeout(() => resolve(Math.random()), 100)
-        );
-    });
+test('should cache value (with arguments)', async t => {
+    const foobar = cache({ timeout: 1000 })(foo);
+
+    const first = await foobar('1', { foo: 'bar' });
+    const second = await foobar('1', { foo: 'bar' });
+
+    t.is(first, second);
+});
+
+test('should not cache value when arguments differ', async t => {
+    const foobar = cache({ timeout: 1000 })(foo);
 
     const first = await foobar('1');
     const second = await foobar('2');
 
     t.not(first, second);
 });
+
+function foo() {
+    return new Promise((resolve) =>
+        setTimeout(() => resolve(Math.random()), 100)
+    );
+}
