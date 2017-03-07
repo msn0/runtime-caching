72f094b6d9518c5f17f5c16634e531789a4ea0b9
Michał Jezierski <ambinanitelo@gmail.com>
2017-03-07 15:37:39 +0100

    cleanup

--- test/class-promise.js
+++ test/class-promise.js
@@ -1,19 +1,5 @@
 import test from 'ava';
-import sinon from 'sinon';
 import FooBar from './fixtures/class-promise';
-
-// test('should call annotated method only once', async t => {
-//     const foobar = new FooBar();
-//     const spy = sinon.spy(foobar, 'bar');
-//
-//     await foobar.foo('1');
-//     console.log(1, foobar.___cache);
-//     await foobar.foo('1');
-//     console.log(2, foobar.___cache);
-//     spy.restore();
-//
-//     t.truthy(spy.calledOnce);
-// });
 
 test('the value for subsequent calls should match', async t => {
     const foobar = new FooBar();
@@ -58,29 +44,3 @@ test('should store under ___cache property of «this» (with arguments)', async 
 
     t.is(await foobar.___cache['foo___{"0":"1","1":{"foo":"bar"},"2":[1,2,3]}'], value);
 });
-
-// test('should remove cached value after given time', t => {
-//     const foobar = new FooBar();
-//     const clock = sinon.useFakeTimers();
-//
-//     const value = await foobar.foo();
-//     const cacheEntry = await foobar.___cache['foo___{}'];
-//     clock.tick(1001);
-//     clock.restore();
-//
-//     t.is(value, cacheEntry);
-//     t.is(foobar.___cache['foo___{}'], undefined);
-// });
-//
-// test('should remove cached value after given time (with arguments)', async t => {
-//     const foobar = new FooBar();
-//     const clock = sinon.useFakeTimers();
-//
-//     const value = await foobar.foo('1', [2, 3]);
-//     const cacheEntry = await foobar.___cache['foo___{"0":"1","1":[2,3]}'];
-//     clock.tick(1001);
-//     clock.restore();
-//
-//     t.is(cacheEntry, value);
-//     t.is(foobar.___cache['foo___{"0":"1","1":[2,3]}'], undefined);
-// });
