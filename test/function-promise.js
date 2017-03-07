05cd1c9f85809bf2686963cda1b67b5a4827440e
Michał Jezierski <ambinanitelo@gmail.com>
2017-03-07 15:14:07 +0100

    should call twice when arguments differ

--- test/function-promise.js
+++ test/function-promise.js
@@ -4,7 +4,7 @@ import { cache } from '../';
 test('should call cached function only once', async t => {
     const foobar = cache({ timeout: 1000 })(function foo() {
         return new Promise((resolve) =>
-            setTimeout(() => resolve(Math.random()), 1000)
+            setTimeout(() => resolve(Math.random()), 100)
         );
     });
 
@@ -14,15 +14,15 @@ test('should call cached function only once', async t => {
     t.is(first, second);
 });
 
-// test('should call twice when arguments differ', async t => {
-//     const foobar = cache({ timeout: 1000 })(function foo() {
-//         return new Promise((resolve) =>
-//             setTimeout(() => resolve(Math.random()), 1000)
-//         );
-//     });
-//
-//     const first = await foobar();
-//     const second = await foobar();
-//
-//     t.is(first, second);
-// });
+test('should call twice when arguments differ', async t => {
+    const foobar = cache({ timeout: 1000 })(function foo() {
+        return new Promise((resolve) =>
+            setTimeout(() => resolve(Math.random()), 100)
+        );
+    });
+
+    const first = await foobar('1');
+    const second = await foobar('2');
+
+    t.not(first, second);
+});
