1bb98e38eb7f80baa106a32c06bfa7440e2e8ab6
Michal Jezierski <ambinanitelo@gmail.com>
2017-10-20 15:50:09 +0200

    demo fixes

--- demo/fork-me-on-github.js
+++ demo/fork-me-on-github.js
@@ -10,7 +10,7 @@ const style = {
 
 export default function () {
     return (
-        <a href="https://github.com/msn0/cache-decorator">
+        <a href="https://github.com/msn0/runtime-caching">
             <img style={ style }
                 src='https://camo.githubusercontent.com/38ef81f8aca64bb9a64448d0d70f1308ef5341ab/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6461726b626c75655f3132313632312e706e67'
                 alt="Fork me on GitHub"
